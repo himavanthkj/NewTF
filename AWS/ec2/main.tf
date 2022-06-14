4a82710fa08fbac3c5a2ed39bbd3ce9d8a99b4c7
himavanthkj <38948602+himavanthkj@users.noreply.github.com>
2022-06-14 16:05:47 +0530

    updating with instance profile

--- AWS/ec2/main.tf
+++ AWS/ec2/main.tf
@@ -5,5 +5,64 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-0022f774911c1d690" # us-east-`
   instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.get_parameters.name
 
+}
+
+resource "aws_iam_role" "get_parameters" {
+    name = "get_parameters"
+    description = "Role to permit ec2 to get parameters from Parameter Store"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "get_parameters" {
+    name = "get_parameters"
+    role = aws_iam_role.get_parameters.name
+
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameters"
+            ],
+            "Resource": [
+                "${var.db_name_arn}",
+                "${var.db_username_arn}",
+                "${var.db_password_arn}"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": [
+                "${var.parameters_key_arn}"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "get_parameters" {
+    name = "get_parameters"
+    role = aws_iam_role.get_parameters.name
 }
